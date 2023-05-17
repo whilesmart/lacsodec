c1ace468f2913471ae620cb9c9a7faee709b0fcc
ExcelDurant <gnopaexcel@yahoo.fr>
2023-05-17 11:11:37 +0100

    Add Swagger documentation to Laravel project using darkaonline/l5-swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+     * @OA\Info(
+     *      version="0.0.1",
+     *      title="Lasodec Documentation",
+     *      description="Lasodec OpenApi description",
+     *      @OA\Contact(
+     *          email="engineering@whilesmart.com"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Demo API Server"
+     * )
+*/
 
 class Controller extends BaseController
 {
