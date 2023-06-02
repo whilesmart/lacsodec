38c47b74c4e6669020ab62a000fc4ecb102ff5b8
Flovet-stack <flovetoloveto@yahoo.com>
2023-06-02 04:35:21 +0100

    started the landing page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,70 @@
+<footer>
+    <div class="top">
+        <div class="con">
+            <div class="sub">
+                <img src="{{ asset('images/logos/lacsodec-logo-white.png') }}" alt="" class="logo">
+                <p>Lorem ipsum dolor sit amet consectetur. Nec sed sit et nunc consequat vitae.</p>
+                <a href="/" class="custom-button secondary"><span>Donate now</span></a>
+            </div>
+            <div class="sub">
+                <h3>Follow us on</h3>
+                <div class="social">
+                    <ul>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-facebook" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-pinterest" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-twitter" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-youtube" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fab fa-linkedin" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="sub">
+                <h3>contact us</h3>
+                <p>
+                    Email: info@lacsodec.org,<br>
+                    Address: NGO Street, <br>
+                    Molyko - Buea
+                </p>
+                <p>Phone: +237 674 58 74 58</p>
+            </div>
+            <div class="sub">
+                <h3>Newsletter</h3>
+                <p>Join our subscribers list to get the latest news and updates delivered directly in your inbox.</p>
+                <form action="">
+                    <input type="text" name="" id="" placeholder="Your email address">
+                    <button type="submit">Sign up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="con">
+            <p>© Copyright Widom Of Life 2020 by Extracoding</p>
+        </div>
+    </div>
+</footer>
