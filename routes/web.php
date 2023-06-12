2d3f2b9144d2dafe68ccf8e334f4efd2e0f91f3a
Flovet-stack <flovetoloveto@yahoo.com>
2023-06-12 13:45:48 +0100

    Add more design pages
    Created the following pages
    
    - Services
    - Publications
    - Wewsroom
    - Impact-stories
    - Events
    - CSO-libarary

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
+
+
 Route::get('/about-us', function () {
     return view('about-us');
 })->name('about-us');
+
 Route::get('/contact-us', function () {
     return view('contact-us');
 })->name('contact-us');
@@ -20,6 +23,31 @@ Route::post('/login', [LoginController::class, 'store'])->name('login.perform');
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/signup', [RegisterController::class, 'store'])->name('register.perform');
 
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/publications', function () {
+    return view('publications');
+})->name('publications');
+
+Route::get('/cso-library', function () {
+    return view('cso-library');
+})->name('cso-library');
+
+Route::get('/impact-stories', function () {
+    return view('impact-stories');
+})->name('impact-stories');
+
+Route::get('/newsroom', function () {
+    return view('newsroom');
+})->name('newsroom');
+
+Route::get('/events', function () {
+    return view('events');
+})->name('events');
+
 Route::group(['middleware' => ['auth']], function () {
     Route::post('/logout', [LoginController::class, 'destroy']);
 });
+
