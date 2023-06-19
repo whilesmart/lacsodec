3b966192cdb73d0f89dcad95ffba6cd22461da64
ExcelDurant <gnopaexcel@yahoo.fr>
2023-06-19 13:44:42 +0100

    Implement expert edit

--- app/Orchid/Screens/ExpertProfileScreen.php
+++ app/Orchid/Screens/ExpertProfileScreen.php
@@ -14,6 +14,8 @@ use Orchid\Screen\Actions\ModalToggle;
 use Orchid\Screen\TD;
 use Orchid\Support\Facades\Toast;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\DropDown;
+use Orchid\Screen\Actions\Link;
 
 class ExpertProfileScreen extends Screen
 {
@@ -67,18 +69,28 @@ class ExpertProfileScreen extends Screen
                 TD::make('location'),
                 TD::make('status'),
                 TD::make('details'),
-                TD::make('Actions')
-        ->alignRight()
-        ->render(function (ExpertProfile $expert) {
-            return Button::make('Delete Expert')
-                ->confirm('After deleting, the expert will be gone forever')
-                ->method('delete', ['expert' => $expert->id]);
-        }),
+                TD::make(__('Actions'))
+                    ->align(TD::ALIGN_CENTER)
+                    ->width('100px')
+                    ->render(fn (ExpertProfile $expert) => DropDown::make()
+                        ->icon('bs.three-dots-vertical')
+                        ->list([
+                            ModalToggle::make('Edit Expert Profile')
+                                ->modal('asyncEditExpertProfile')
+                                ->method('edit')
+                                ->icon('bs.pencil')
+                                ->asyncParameters(['expert' => $expert->id]),
+
+                            Button::make('Delete Expert')
+                                ->confirm('After deleting, the expert will be gone forever')
+                                ->icon('bs.trash')
+                                ->method('delete', ['expert' => $expert->id])
+                        ])),
             ]),
 
             Layout::modal('ExpertProfile', Layout::rows([
                 Select::make('user_id')
-                ->fromModel(User::class, 'email', 'id')
+                    ->fromModel(User::class, 'email', 'id')
                     ->title('Select user')
                     ->help('Select the user to be an expert')
                     ->required(),
@@ -87,7 +99,7 @@ class ExpertProfileScreen extends Screen
                     ->placeholder('Enter Expert location')
                     ->help('The location of the expert')
                     ->required(),
-                    Input::make('status')
+                Input::make('status')
                     ->title('Expert status')
                     ->placeholder('Enter Expert status')
                     ->help('The status of the expert')
@@ -100,6 +112,34 @@ class ExpertProfileScreen extends Screen
             ]))
                 ->title('Create Expert Profile')
                 ->applyButton('Add Expert'),
+
+            Layout::modal('asyncEditExpertProfile', [
+                Layout::rows([
+                    Input::make('expert.id')->canSee(false)->required(),
+                    Select::make('expert.user_id')
+                        ->fromModel(User::class, 'email', 'id')
+                        ->title('Select user')
+                        ->help('Select the user to be an expert')
+                        ->required(),
+                    Input::make('expert.location')
+                        ->title('Location')
+                        ->placeholder('Enter Expert location')
+                        ->help('The location of the expert')
+                        ->required(),
+                    Input::make('expert.status')
+                        ->title('Expert status')
+                        ->placeholder('Enter Expert status')
+                        ->help('The status of the expert')
+                        ->required(),
+                    TextArea::make('expert.details')
+                        ->title('details')
+                        ->placeholder('Enter Expert details')
+                        ->rows(5)
+                        ->required(),
+                ]),
+            ])->async('asyncGetExpert')
+                ->title('Edit Expert Profile')
+                ->applyButton('Edit Expert')
         ];
     }
 
@@ -114,7 +154,7 @@ class ExpertProfileScreen extends Screen
         $user = User::findOrFail($fields['user_id']);
 
         $experts = ExpertProfile::where('user_id', $fields['user_id'])->count();
-        if($experts > 0) {
+        if ($experts > 0) {
             Toast::error('The selected user is already an expert');
             return;
         }
@@ -125,11 +165,33 @@ class ExpertProfileScreen extends Screen
             'status' => $fields['status'],
         ]);
 
-        Toast::success($user->name. ' is now an expert');
+        Toast::success($user->name . ' is now an expert');
     }
 
     public function delete(ExpertProfile $expert)
     {
         $expert->delete();
+    }
+
+    public function edit(Request $request)
+    {
+        $request->validate([
+            'expert.user_id' => ['required', 'integer'],
+            'expert.location' => ['required', 'string'],
+            'expert.details' => ['required', 'string'],
+            'expert.status' => ['required', 'string']
+        ]);
+        $expert = ExpertProfile::where('user_id', $request->expert['user_id'])->first();
+        $expert->location = $request->expert['location'];
+        $expert->details = $request->expert['details'];
+        $expert->status = $request->expert['status'];
+        Toast::success('Expert profile updated successfully');
+    }
+
+    public function asyncGetExpert(ExpertProfile $expert): array
+    {
+        return [
+            'expert' => $expert
+        ];
     }
 }
