5197538835920701f5da5960de112813c02d38ec
ExcelDurant <gnopaexcel@yahoo.fr>
2023-06-21 11:56:33 +0100

    Add more properties to CSO model and migration

---
+++ app/Models/CsoActivityDomain.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CsoActivityDomain extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'domain',
+        'cso_id',
+    ];
+
+    public function cso(): BelongsTo
+    {
+        return $this->belongsTo(Cso::class);
+    }
+}
