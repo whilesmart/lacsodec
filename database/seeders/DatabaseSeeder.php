71547214969ad2136cd46b7b7fca93a1b92fb826
ExcelDurant <gnopaexcel@yahoo.fr>
2023-06-25 12:31:19 +0100

    Create cso and expert factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CsoActivitySeeder::class
+            CsoActivitySeeder::class,
+            CsoSeeder::class,
+            ExpertProfileSeeder::class
         ]);
     }
 }
