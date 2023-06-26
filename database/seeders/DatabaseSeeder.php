0e29d658ce177ffe916cfb77c98cfb238ef10c55
ExcelDurant <gnopaexcel@yahoo.fr>
2023-06-26 10:01:09 +0100

    Create articles factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CsoActivitySeeder::class,
             CsoSeeder::class,
-            ExpertProfileSeeder::class
+            ExpertProfileSeeder::class,
+            ArticleSeeder::class
         ]);
     }
 }
