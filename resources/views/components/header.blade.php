8508d486017626a3772eda4d82fca7df37a6ef18
Flovet-stack <flovetoloveto@yahoo.com>
2023-06-27 12:43:26 +0100

    all pages are now fully responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -57,6 +57,7 @@
                     <li><a href="{{ route('grants') }}">grants</a></li>
                     <li><a href="{{ route('blog') }}">blog</a></li>
                     <li><a href="{{ route('lodge') }}">lodge</a></li>
+                    <li><a href="{{ route('lodge') }}" class="mobile-only">Contact us</a></li>
                 </ul>
             </nav>
             <div class="right">
