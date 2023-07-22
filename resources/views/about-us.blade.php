49de998537d1f3cdd0cd0e9df3bc2678a48b22ed
Flovet-stack <flovetoloveto@yahoo.com>
2023-07-22 10:04:45 +0100

    updated the classes of the member cards on the about us page so they have the correct styling

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -2,9 +2,9 @@
     <div class="about-us-page">
         <!-- breadcrumb -->
         @component('components.breacrumb')
-        @slot('current')
-        {{__('home.About Us')}}
-        @endslot
+            @slot('current')
+                {{ __('home.About Us') }}
+            @endslot
         @endcomponent
 
         <section class="about-section-1">
@@ -14,9 +14,11 @@
                         <img src="{{ asset('images/home-img-1.png') }}" alt="" />
                         <img src="{{ asset('images/home-img-2.png') }}" alt="" />
                     </div>
-                    <p><b>{{__('about.WHO WE ARE')}}</b></p>
-                    <p>{{__('about.The LUKMEF Africa Civil Society Development Centre (LACSODEC) is a Civil Society Organization (CSO) service providing agency facilitating knowledge acquisition, dialogue among and between CSOs and their networks, mutualization of resources, information sharing and promotion of fair access to resources by CSOs of all categories (levels) using the principle of need, equality and equity with the population as final beneficiaries in mind.')}}</p>
-                    <p>{{__('about.LACSODEC was founded in {year} by Tanyi Christian under the LUKMEF vision for Sustainable Development and Empowerment of communities and other local civil society actors to improve on service delivery. LACSODEC is located in NGO Street, Molyko – Buea, in the SW Region of Cameroon with registration number {insert here}.')}}</p>
+                    <p><b>{{ __('about.WHO WE ARE') }}</b></p>
+                    <p>{{ __('about.The LUKMEF Africa Civil Society Development Centre (LACSODEC) is a Civil Society Organization (CSO) service providing agency facilitating knowledge acquisition, dialogue among and between CSOs and their networks, mutualization of resources, information sharing and promotion of fair access to resources by CSOs of all categories (levels) using the principle of need, equality and equity with the population as final beneficiaries in mind.') }}
+                    </p>
+                    <p>{{ __('about.LACSODEC was founded in {year} by Tanyi Christian under the LUKMEF vision for Sustainable Development and Empowerment of communities and other local civil society actors to improve on service delivery. LACSODEC is located in NGO Street, Molyko – Buea, in the SW Region of Cameroon with registration number {insert here}.') }}
+                    </p>
                 </div>
                 <div class="right">
                     <img src="{{ asset('images/home-img-2.png') }}" alt="" />
@@ -27,10 +29,10 @@
         <section class="about-section-2">
             <div class="con">
                 <div class="left">
-                    <h1>{{__('about.Our vision')}}</h1>
-                    <p>{{__('about.To build a dependable and impactful Civil Society in Africa')}}</p>
-                    <h1>{{__('about.Our Mission')}}</h1>
-                    <p>{{__('about.Mission is to strengthen the institutional and operational capacity of CSOs, improve their operating environment in order to amplify their voices, and improve performance in addressing diverse issues of human development at local, national and regional level in West Africa and the Lake Chad Basin Countries using a rights-based approach.')}}
+                    <h1>{{ __('about.Our vision') }}</h1>
+                    <p>{{ __('about.To build a dependable and impactful Civil Society in Africa') }}</p>
+                    <h1>{{ __('about.Our Mission') }}</h1>
+                    <p>{{ __('about.Mission is to strengthen the institutional and operational capacity of CSOs, improve their operating environment in order to amplify their voices, and improve performance in addressing diverse issues of human development at local, national and regional level in West Africa and the Lake Chad Basin Countries using a rights-based approach.') }}
                     </p>
                     <!-- <ul>
                         <li>
@@ -53,8 +55,8 @@
                 </div>
                 <div class="right">
                     <div class="members">
-                        <h3>{{$members}}</h3>
-                        <p>{{__('home.Members')}}</p>
+                        <h3>{{ $members }}</h3>
+                        <p>{{ __('home.Members') }}</p>
                     </div>
                     <img src="{{ asset('images/about-building.png') }}" alt="" />
                 </div>
@@ -66,39 +68,42 @@
         <section class="about-section-3">
             <div class="con">
                 <div class="left">
-                    <h1>{{__('about.WHY WE EXIST')}}</h1>
+                    <h1>{{ __('about.WHY WE EXIST') }}</h1>
                     <ul>
                         <li>
                             <i class="fa fa-arrow-right" aria-hidden="true"></i>
                             <div class="content">
-                                <h3>{{__('about.The 1990 law on Associations and the 1999 Law on NGOs are the two fundamental legal instruments used by the civil society in Cameroon.')}}</h3>
+                                <h3>{{ __('about.The 1990 law on Associations and the 1999 Law on NGOs are the two fundamental legal instruments used by the civil society in Cameroon.') }}
+                                </h3>
                             </div>
                         </li>
                         <li>
                             <i class="fa fa-arrow-right" aria-hidden="true"></i>
                             <div class="content">
-                                <h3>{{__('about.Within the framework of these two laws, over 55,602 organizations are registered in the books of the ministry of territorial administration at sub divisional (DO) and divisional levels (SDO) in Cameroon (Ref CSOSI-2019 by USAID).')}}</h3>
+                                <h3>{{ __('about.Within the framework of these two laws, over 55,602 organizations are registered in the books of the ministry of territorial administration at sub divisional (DO) and divisional levels (SDO) in Cameroon (Ref CSOSI-2019 by USAID).') }}
+                                </h3>
                             </div>
                         </li>
                         <li>
                             <i class="fa fa-arrow-right" aria-hidden="true"></i>
                             <div class="content">
-                                <h3>{{__('about.According to the National Governance Program, approximately 500,000 grassroots community organizations, private associations, common initiative groups, development committees, and economic interest groups, were thought to exist in 2019.')}}</h3>
+                                <h3>{{ __('about.According to the National Governance Program, approximately 500,000 grassroots community organizations, private associations, common initiative groups, development committees, and economic interest groups, were thought to exist in 2019.') }}
+                                </h3>
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div class="right">
                     <div class="tabs">
-                        <button class="tab active">{{__('about.WHAT WE DO')}}</button>
-                        <button class="tab">{{__('about.OUR APPROACH')}}</button>
+                        <button class="tab active">{{ __('about.WHAT WE DO') }}</button>
+                        <button class="tab">{{ __('about.OUR APPROACH') }}</button>
                     </div>
                     <div class="content tab-content">
-                        <p>{{__('about.We facilitate knowledge acquisition, dialogue among and between CSOs and their networks, mutualization of resources, information sharing and promotion of fair access to resources by CSOs of all categories (levels) using the principle of need, equality and equity with the population as final beneficiaries in mind.')}}'
+                        <p>{{ __('about.We facilitate knowledge acquisition, dialogue among and between CSOs and their networks, mutualization of resources, information sharing and promotion of fair access to resources by CSOs of all categories (levels) using the principle of need, equality and equity with the population as final beneficiaries in mind.') }}'
                         </p>
                     </div>
                     <div class="content tab-content" style="display: none;">
-                        <p>{{__('about.Mission is to strengthen the institutional and operational capacity of CSOs, improve their operating environment in order to amplify their voices, and improve performance in addressing diverse issues of human development at local, national and regional level in West Africa and the Lake Chad Basin Countries using a rights-based approach.')}}'
+                        <p>{{ __('about.Mission is to strengthen the institutional and operational capacity of CSOs, improve their operating environment in order to amplify their voices, and improve performance in addressing diverse issues of human development at local, national and regional level in West Africa and the Lake Chad Basin Countries using a rights-based approach.') }}'
                         </p>
                     </div>
                 </div>
@@ -107,7 +112,7 @@
 
         <section class="team-section">
             <div class="con">
-                <h2>{{__('about.Our team')}}</h2>
+                <h2>{{ __('about.Our team') }}</h2>
 
                 <!-- WAITING FOR NAMES AND IMAGES OF TEAM MEMBERS AND THEIR POSTS
                     
@@ -137,27 +142,27 @@
                         <h4>Chita Brine Chita</h4>
                         <p>IT Manager</p>
                     </div>
-                    <div class="member">
+                    <div class="member-card">
                         <img src="{{ asset('images/team/team-member-6.png') }}" alt="">
                         <h4>Kolle</h4>
                         <p>Logistics Manager</p>
                     </div>
-                    <div class="member">
+                    <div class="member-card">
                         <img src="{{ asset('images/team/team-member-6.png') }}" alt="">
                         <h4>Efeti Zita</h4>
                         <p>Facility Manager</p>
                     </div>
-                    <div class="member">
+                    <div class="member-card">
                         <img src="{{ asset('images/team/team-member-6.png') }}" alt="">
                         <h4>Olive Bobga</h4>
                         <p>Gender Expert</p>
                     </div>
-                    <div class="member">
+                    <div class="member-card">
                         <img src="{{ asset('images/team/team-member-6.png') }}" alt="">
                         <h4>Simine Borner</h4>
                         <p>Child Protection Expert</p>
                     </div>
-                    <div class="member">
+                    <div class="member-card">
                         <img src="{{ asset('images/team/team-member-6.png') }}" alt="">
                         <h4>Wabo kevin</h4>
                         <p>MEAL Officer</p>
