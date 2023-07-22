037db083160602927fac0f3b6cc620374d822f6a
ExcelDurant <gnopaexcel@yahoo.fr>
2023-07-22 21:59:36 +0100

    Add content to donate page

--- lang/en/donate.php
+++ lang/en/donate.php
@@ -11,5 +11,12 @@ return [
     */
 
     'Donate' => 'Donate',
+    'Bank details' => 'Bank details',
+    'Bank code' => 'Bank code',
+    'Branch Code' => 'Branch Code',
+    'Account Number' => 'Account Number',
+    'RIB Key' => 'RIB Key',
+    'IBAN Code' => 'IBAN Code',
+    'Swift Code' => 'Swift Code',
 
 ];
